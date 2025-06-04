24f0a2ff48d28253badc4ee0be5a667f9ab48317
blitherland <blit0001@student.monash.edu.au>
2025-06-04 18:12:31 +1000

    Created the base classes for a solver
    MazeSolver is an abstract class that all solver types will use
    
    DfsSolver will solve a maze given a start and finish point.

---
+++ mazegenerator/src/main/java/com/mazegen/DfsSolver.java
@@ -0,0 +1,13 @@
+package com.mazegen;
+
+import java.util.ArrayList;
+
+public class DfsSolver extends MazeSolver {
+    @Override
+    public ArrayList<Integer> solveMaze(AdjMatrix maze, int start, int finish) {
+        // TODO Auto-generated method stub
+        
+
+        return super.solveMaze(maze);
+    }
+}
---
+++ mazegenerator/src/main/java/com/mazegen/MazeSolver.java
@@ -0,0 +1,10 @@
+package com.mazegen;
+
+import java.util.ArrayList;
+
+public abstract class MazeSolver{
+    public ArrayList<Integer> solveMaze(AdjMatrix maze, int start, int finish){
+        return null;
+    }
+
+}
